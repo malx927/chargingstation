ce2bbb4d81797a1f6f48eb6f6b4d5cc91b3fc842
mlx927 <5971158@qq.com>
2019-07-23 08:53:11 +0800

    update

--- chargingorder/api/urls.py
+++ chargingorder/api/urls.py
@@ -1,5 +1,7 @@
 #-*-coding:utf-8-*-
 from django.conf.urls import url
+
+from chargingorder.api.views import OrderDayStats
 from stationmanager.api.views import ChargingPileListAPIView, AreaCodeListAPIView
 
 __author__ = 'malixin'
@@ -8,5 +10,6 @@ __author__ = 'malixin'
 urlpatterns = [
     url(r'^account_balance/$', ChargingPileListAPIView.as_view(), name='charging-pile-list'),
     url(r'^areacodelist/$', AreaCodeListAPIView.as_view(), name='area-code-list'),
+    url(r'^orderdaystats/$', OrderDayStats.as_view(), name='order-day-stats'),
 ]
 
