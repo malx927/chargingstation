89379ad030891fce27fd733547e9543195f16c40
mlx927 <5971158@qq.com>
2019-09-28 16:59:01 +0800

    update

--- chargingorder/templatetags/chargingorder.py
+++ chargingorder/templatetags/chargingorder.py
@@ -4,9 +4,9 @@ from django.template import Library
 register = Library()
 
 
-# @register.simple_tag
-# def gen_role_url(request, rid):
-#     params = request.GET.copy()
-#     params._mutable = True
-#     params['rid'] = rid
-#     return params.urlencode()
+@register.simple_tag
+def is_AC(order):
+    if order.charg_pile.pile_type.id == 5 or order.charg_pile.pile_type.id == 6:
+        return True
+    else:
+        return False
