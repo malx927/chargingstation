c3a93437ec8ea08ee47f38b4247406be6aa1ed14
mlx927 <5971158@qq.com>
2019-10-10 15:31:27 +0800

    update

--- echargenet/signals.py
+++ echargenet/signals.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import logging
+
 from django.db.models.signals import post_init, post_save
 from django.dispatch import receiver
 
@@ -14,8 +16,9 @@ def connector_status_init(instance, **kwargs):
 
 @receiver(post_save, sender=ConnectorInfo)
 def notification_connector_status(sender, instance, created, update_fields, **kwargs):
-    print(created, instance.org_Status, instance.Status)
+    logging.info("{}-{}-{}".format(created, instance.org_Status, instance.Status))
     if not created and instance.org_Status != instance.Status:
         echarge = EchargeNet(settings.MQTT_REDIS_URL, settings.MQTT_REDIS_PORT)
         status = echarge.notification_station_status(instance.ConnectorID, instance.Status)
+        logging.info("signal ret value:{}".format(status))
         print("signal ret value:", status)
