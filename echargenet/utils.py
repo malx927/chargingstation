c1fa3d537fed1832f785cc0211e81183cff1265e
mlx927 <5971158@qq.com>
2019-10-22 13:54:23 +0800

    update

--- echargenet/utils.py
+++ echargenet/utils.py
@@ -20,9 +20,9 @@ unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 def get_order_status(charg_status):
     if charg_status is None:
         return 5
-    if 5 >= charg_status >= 1:
+    if 3 >= charg_status >= 1:
         return 1        # 启动中
-    elif charg_status == 6:
+    elif 6 >= charg_status >= 4:
         return 2        # 充电中
     elif charg_status == 7:
         return 3        # 停止中
