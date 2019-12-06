112b5e055c7a3c0aa846088f59a57d85cdc2ff18
malx927 <5971158@qq.com>
2019-12-06 22:51:44 +0800

    update

---
+++ client/static/client/dist/js/client.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('ul.main-menu li a').each(function () {
+        if ($($(this))[0].href == String(window.location))
+            $(this).parent().addClass('active');
+    });
+})
