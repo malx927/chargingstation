9270176445414793fe4c721a3e95e7d5409d49a3
malx927 <5971158@qq.com>
2019-12-06 22:33:11 +0800

    update

--- client/urls.py
+++ client/urls.py
@@ -2,8 +2,10 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import index
+from .views import index, login
+
 urlpatterns = [
     url(r'^$', index),  # 客户端入口
+    url(r'^login/$', login),  # 登录窗口
 
 ]
--- client/views.py
+++ client/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
+def login(request):
+    return render(request, template_name="client/login.html")
+
+
 def index(request):
     return render(request, template_name="client/index.html")
