07ea17531f920cb4a361e5a13f3e1a8e7f193dbf
mlx927 <5971158@qq.com>
2020-11-18 14:30:39 +0800

    update

--- wxchat/adminx.py
+++ wxchat/adminx.py
@@ -47,8 +47,8 @@ class UserInfoAdmin(object):
     """
     充电(微信)用户
     """
-    list_display = ['name', 'nickname', 'openid', 'telephone', 'total_money', 'consume_money', 'account_balance', 'is_freeze', 'balance_reset']
-    search_fields = ['name', 'openid', 'telephone', 'nickname', 'pile_sn']
+    list_display = ['name', 'nickname', 'openid', 'telephone', 'car_type', 'total_money', 'consume_money', 'account_balance', 'is_freeze', 'balance_reset']
+    search_fields = ['name', 'openid', 'telephone', 'nickname', 'pile_sn', 'car_type']
     list_filter = ['user_type', 'seller']
     list_per_page = 50
     reversion_enable = True
