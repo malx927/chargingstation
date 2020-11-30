f0eb6fd91b59f8cf5d218d1024850511b4559d41
mlx927 <5971158@qq.com>
2020-11-30 12:11:55 +0800

    update

--- bill/adminx.py
+++ bill/adminx.py
@@ -11,7 +11,7 @@ class InvoiceTitleAdmin(object):
     """
     发票抬头信息
     """
-    list_display = ["name", "title", "category", "tax_number", "address", "telephone", "bank_account", "email", "is_write"]
+    list_display = ["name", "title", "category", "tax_number", "address", "telephone", "bank_account", "email", "is_write", "add_time"]
     search_fields = ["title", "name", "openid", "telephone"]
     list_filter = ['is_write']
     model_icon = 'fa fa-random'
